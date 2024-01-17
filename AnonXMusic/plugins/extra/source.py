c25b9063ff850434cd95724565fa880e6efd8d91
Almortagel <105941198+Almortage@users.noreply.github.com>
2024-01-17 23:35:33 +0200

    Update source.py

--- AnonXMusic/plugins/extra/source.py
+++ AnonXMusic/plugins/extra/source.py
@@ -15,7 +15,7 @@ from random import  choice, randint
 
 @app.on_message(
     filters.command(["سورس مين","سورس","السورس","سورسي", "Almortagel"])
-    & filters.edited
+    & filters.group
 )
 async def huhh(client: Client, message: Message):
     await message.reply_photo(
