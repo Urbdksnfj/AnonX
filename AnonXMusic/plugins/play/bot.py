feb36d59eba7754783eaaac038106a1573496074
Almortagel <105941198+Almortage@users.noreply.github.com>
2024-01-20 11:28:33 +0200

    Update bot.py

--- AnonXMusic/plugins/play/bot.py
+++ AnonXMusic/plugins/play/bot.py
@@ -45,7 +45,8 @@ async def caesar_bot(client, message):
     ])
 
     await message.reply_text(
-        text=f"{bar}(https://t.me/{bot_username}?startgroup=True)",
+        text=f"{bar}",
+        disable_web_page_preview=True,
         reply_markup=keyboard
     )
     
