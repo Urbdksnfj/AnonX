8b66b26fadad60d5e139b8948ed6fe5d0d480945
Almortagel <105941198+Almortage@users.noreply.github.com>
2024-01-20 11:48:12 +0200

    Update sorse.py

--- AnonXMusic/plugins/play/sorse.py
+++ AnonXMusic/plugins/play/sorse.py
@@ -36,7 +36,7 @@ async def huhh(client: Client, message: Message):
         ],
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"),
+                text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",
                 ],
 
             ]
