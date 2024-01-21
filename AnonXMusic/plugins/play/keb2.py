3e36bd9b1996c7654b036a8e321000cdb79868a5
Almortagel <105941198+Almortage@users.noreply.github.com>
2024-01-21 11:06:26 +0200

    Update keb2.py

--- AnonXMusic/plugins/play/keb2.py
+++ AnonXMusic/plugins/play/keb2.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters, raw, utils
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, Message, User, ChatPrivileges, ReplyKeyboardRemove, CallbackQuery
 from pyrogram import enums
+from AnonXMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 from pyrogram.enums import ChatType, ChatMemberStatus, ParseMode, ChatMemberStatus
 import aiohttp
 import asyncio
