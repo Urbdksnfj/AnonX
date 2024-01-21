dc8d2dd86eb8eb3395cc8a03eadcd5648b686052
Almortagel <105941198+Almortage@users.noreply.github.com>
2024-01-21 16:34:47 +0200

    Update keb2.py

--- AnonXMusic/plugins/play/keb2.py
+++ AnonXMusic/plugins/play/keb2.py
@@ -364,5 +364,4 @@ async def upbkgt(client: Client, message: Message):
     await message.reply_text(
         text="""تم حذف الكيبورد بنجاح""",
         reply_markup=ReplyKeyboardRemove()
-        ),
     )
