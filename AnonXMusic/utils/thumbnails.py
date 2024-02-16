758f57bb8e19709f6f3ea355f0278219b457ea37
Almortagel <105941198+Almortage@users.noreply.github.com>
2024-02-16 22:49:23 +0200

    Update thumbnails.py

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -8,7 +8,7 @@ import aiohttp
 from PIL import (Image, ImageDraw, ImageEnhance, ImageFilter,
                  ImageFont, ImageOps)
 from youtubesearchpython.__future__ import VideosSearch
-from AnonXMusic import np
+from AnonXMusic import app
 from config import YOUTUBE_IMG_URL
 
 
